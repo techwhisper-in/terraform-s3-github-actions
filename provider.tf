4646509fb8332d4aa3274ac632e6cb7425fe27bd
Ankit Kumar <technicalcodelab@gmail.com>
2025-09-03 11:29:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket         = "store-statefile-terraform-1999"
-    region         = "us-east-2"
-    key            = "${var.BucketName}/terraform.tfstate"
-    encrypt = true
-  }
+  backend "s3" {}
   required_version = ">=0.13.0"
   required_providers {
     aws = {
